0ecca7ff133932dbcc3be864f3e3593462ebb89b
Ben Brown <bencampbellbrown@gmail.com>
2025-11-28 01:28:14 +0000

    Create Week7Code.dart

---
+++ lib/models/Week7Code.dart
@@ -0,0 +1,66 @@
+import 'sandwich.dart';
+import 'package:sandwich_shop/repositories/pricing_repository.dart';
+
+class Cart {
+  final Map<Sandwich, int> _items = {};
+
+  // Returns a read-only copy of the items and their quantities
+  Map<Sandwich, int> get items => Map.unmodifiable(_items);
+
+  void add(Sandwich sandwich, {int quantity = 1}) {
+    if (_items.containsKey(sandwich)) {
+      _items[sandwich] = _items[sandwich]! + quantity;
+    } else {
+      _items[sandwich] = quantity;
+    }
+  }
+
+  void remove(Sandwich sandwich, {int quantity = 1}) {
+    if (_items.containsKey(sandwich)) {
+      final currentQty = _items[sandwich]!;
+      if (currentQty > quantity) {
+        _items[sandwich] = currentQty - quantity;
+      } else {
+        _items.remove(sandwich);
+      }
+    }
+  }
+
+  void clear() {
+    _items.clear();
+  }
+
+  double get totalPrice {
+    final pricingRepository = PricingRepository();
+    double total = 0.0;
+
+    for (Sandwich sandwich in _items.keys) {
+      int quantity = _items[sandwich]!;
+      total += pricingRepository.calculatePrice(
+        quantity: quantity,
+        isFootlong: sandwich.isFootlong,
+      );
+    }
+
+    return total;
+  }
+
+  bool get isEmpty => _items.isEmpty;
+
+  int get length => _items.length;
+
+  int get countOfItems {
+    int total = 0;
+    for (Sandwich sandwich in _items.keys) {
+      total += _items[sandwich]!;
+    }
+    return total;
+  }
+
+  int getQuantity(Sandwich sandwich) {
+    if (_items.containsKey(sandwich)) {
+      return _items[sandwich]!;
+    }
+    return 0;
+  }
+}
