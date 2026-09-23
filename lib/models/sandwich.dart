e013e8bb8137ba771d6e87b889c3678ffbe0bde2
manighahrmani <mashapicasso@gmail.com>
2026-09-24 00:55:12 +0100

    Add image

---
+++ lib/models/sandwich.dart
@@ -0,0 +1,15 @@
+class Sandwich {
+  final String id;
+  final String name;
+  final String description;
+  final double price;
+  final String imagePath;
+
+  Sandwich({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.imagePath,
+  });
+}
